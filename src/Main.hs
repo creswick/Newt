46a883472a7b51d6c1a8520f0aa78c7c20194035
Rogan Creswick <creswick@galois.com>
2011-05-02 21:49:52 -0700

    now prints tags if no output file is given, handles bad input more gracefully

--- src/Main.hs
+++ src/Main.hs
@@ -3,12 +3,34 @@ module Main where
 import Data.List ( elemIndex, partition )
 import Data.Array (elems)
 import Data.Maybe ( mapMaybe )
+import qualified Data.Set as Set
 
 import System.Environment ( getArgs )
 import Text.Regex.PCRE ( Regex )
 import Text.Regex.PCRE.String ( compile, compUngreedy, execBlank,
                                 MatchOffset )
 import Text.Regex.Base.RegexLike ( matchAllText )
+
+main :: IO ()
+main = do args <- getArgs
+          let (rawPairs, files) = partition isPair args
+              table             = mapMaybe strToPair rawPairs
+              replacement input output = replaceFile simpleTag table input output
+
+          case (length files) of
+            2 -> replacement (head files) (files!!1)
+            1 -> printTags simpleTag (head files)
+            _ -> printHelp
+
+printTags :: Tag a => a -> FilePath -> IO ()
+printTags tag file = do content <- readFile file
+                        regexp  <- tagRegex tag
+                        let matches = map (head . elems) (matchAllText regexp content)
+                            toStr (s, _) = (stripTag tag) s
+                        mapM_ putStrLn $ Set.toList $ Set.fromList $ map toStr matches
+
+printHelp :: IO ()
+printHelp = putStrLn "Usage: newt <inFile> [<outFile> [key=value]]"
 
 class Tag a where
     tagRegex :: a -> IO Regex
@@ -28,14 +50,8 @@ simpleTag = TagSyntax "<<<" ">>>"
 mkRegex :: String -> String -> IO Regex
 mkRegex front back = do res <- makeRegex (front++".+"++back)
                         case res of
-                          Left (offset, err) -> error err
-                          Right regex        -> return regex
-
-main :: IO ()
-main = do args <- getArgs
-          let (rawPairs, files) = partition isPair args
-              table             = mapMaybe strToPair rawPairs
-          replaceFile simpleTag table (head files) (files!!1)
+                          Left ( _ , err) -> error err
+                          Right regex     -> return regex
 
 isPair :: String -> Bool
 isPair str = '=' `elem` str
