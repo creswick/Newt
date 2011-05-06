fd8775de64ad844b9584bc1bbf0b747dd3b9d534
Rogan Creswick <creswick@galois.com>
2011-05-06 15:21:50 -0700

    created separate library and executable cabal file entries

--- src/Newt/Inputs.hs
+++ src/Newt/Inputs.hs
@@ -2,15 +2,17 @@ module Newt.Inputs where
 
 import System.Directory ( doesDirectoryExist, doesFileExist )
 
+import Control.Monad.Error ( ErrorT, throwError, liftIO )
+
 data InputKind = TxtFile FilePath
                | Directory FilePath
                  -- | TarGz FilePath
                  -- | Zip FilePath
                  deriving (Show)
 
-inputKind :: FilePath -> ErrorT String (IO InputKind)
-inputKind pth = do dirExists  <- doesDirectoryExist pth
-                   fileExists <- doesFileExist pth
+inputKind :: FilePath -> ErrorT String IO InputKind
+inputKind pth = do dirExists  <- liftIO $ doesDirectoryExist pth
+                   fileExists <- liftIO $ doesFileExist pth
                    case dirExists of
                      True  -> return (Directory pth)
                      False -> case fileExists of
